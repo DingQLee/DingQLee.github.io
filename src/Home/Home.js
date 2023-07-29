cb8a80b9af122d112b3dfd37ff27dbb13f4e53a8
DingQLee <conroylee1213@gmail.com>
2023-07-29 18:18:10 +0800

    update build for gh-pages

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import './Home.css'
+import Fade from 'react-reveal/Fade'
 
 export const Home = () => {
   let year = new Date().getFullYear()
@@ -7,22 +8,24 @@ export const Home = () => {
   return (
     <div className='home-div' id='home'>
 
-      <div className='header-div'>
-        <h1>MarioCart</h1>
-      </div>
-      <div className='float-div'></div>
-      <div className='header-div'>
-        <h1>Age <span>I am {myYear} year old</span></h1>
-      </div>
-      <div className='header-div'>
-        <h1>University <span>The Chinese University of Hong Kong</span></h1>
-      </div>
-      <div className='header-div'>
-        <h1>Major <span>Electronic Engineering</span></h1>
-      </div>
-      <div className='header-div'>
-        <h1>Programmer <span>Web Design Enthusiast</span></h1>
-      </div>
+      <Fade left cascade>
+        <div className='header-div'>
+          <h1>MarioCart</h1>
+        </div>
+        <div className='float-div'></div>
+          <div className='header-div'>
+            <h1>Age <span>I am {myYear} year old</span></h1>
+          </div>
+        <div className='header-div'>
+          <h1>University <span>The Chinese University of Hong Kong</span></h1>
+        </div>
+        <div className='header-div'>
+          <h1>Major <span>Electronic Engineering</span></h1>
+        </div>
+        <div className='header-div typing'>
+          <h1>Programmer <span>Web Design Enthusiast</span></h1>
+        </div>
+      </Fade>
       
     </div>
   )
