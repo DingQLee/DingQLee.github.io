6fa75b363a6eca5a920810e4f8c5f34a10b8e801
DingQLee <conroylee1213@gmail.com>
2023-07-29 19:38:50 +0800

    Updates

---
+++ precache-manifest.1b91e3024d3729845e854cff5cc2f91c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e73a42f7acf03a78f39",
+    "url": "/static/css/main.152605e3.chunk.css"
+  },
+  {
+    "revision": "6e73a42f7acf03a78f39",
+    "url": "/static/js/main.5fcf7424.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f0a44f342db04583c25c",
+    "url": "/static/js/2.3b28472c.chunk.js"
+  },
+  {
+    "revision": "b6c0d73d216167e62d9ea5f91b6072ed",
+    "url": "/static/media/pattern.b6c0d73d.png"
+  },
+  {
+    "revision": "895a91dc1471b70de27f44214a006dde",
+    "url": "/static/media/potato.895a91dc.png"
+  },
+  {
+    "revision": "cac197c558e10af5ab34d12cc242f4e3",
+    "url": "/index.html"
+  }
+];
