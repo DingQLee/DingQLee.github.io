7f0aaa0b1b3fa71c623a8f47b7de8a98ff8e5451
DingQLee <conroylee1213@gmail.com>
2023-07-31 23:16:16 +0800

    removed loader

--- src/App.js
+++ src/App.js
@@ -6,36 +6,13 @@ import { Hobbies } from './Hobbies/Hobbies'
 import { Contact } from './Contact/Contact'
 
 function App() {
-  const [loading, setLoading] = useState(false)
-
-  const Loader = () => {
-    return (
-    <div className='loader-div'>
-      <h1>Loading into Conroy Lee's Portfolio</h1>
-    </div>
-    )
-  }
-
-  useEffect(() => {
-    setLoading(true)
-    setTimeout(() => {
-      setLoading(false)
-    }, 8000)
-  }, [])
 
   return (
     <div className="App">
-      {
-      loading ? 
-        <Loader/> 
-      :
-        <div>
-          <Navbar />
-          <Home />
-          <Hobbies />
-          <Contact />
-        </div>
-      }
+      <Navbar />
+      <Home />
+      <Hobbies />
+      <Contact />
     </div>
   )
 }
